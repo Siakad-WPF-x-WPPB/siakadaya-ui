3102550955bb2a6a4361c9e44092ad4c84d62806
Raihan <raihanm854@gmail.com>
2025-05-08 20:37:10 +0700

    feat: migrasi tabel

--- database/migrations/2025_05_05_074254_create_persetujuan_frs_table.php
+++ database/migrations/2025_05_05_074254_create_persetujuan_frs_table.php
@@ -12,11 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('persetujuan_frs', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('id_frs')->constrained('frs')->onDelete('cascade');
-            $table->foreignId('id_jadwal')->constrained('jadwal')->onDelete('cascade');
-            $table->foreignId('nip')->constrained('dosen')->onDelete('cascade');
-            $table->foreignId('kode_matakuliah')->constrained('matakuliah')->onDelete('cascade');
+            $table->uuid('id')->primary();
+            $table->foreignId('frs_id')->constrained('frs')->onDelete('cascade');
+            $table->foreignId('jadwal_id')->constrained('jadwal')->onDelete('cascade');
             $table->enum('status', ['disetujui', 'ditolak', 'pending'])->default('pending');
             $table->timestamps();
         });
