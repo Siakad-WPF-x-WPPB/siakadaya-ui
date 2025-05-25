9678f6cd854adbec70d2dbfe3399f116d9582705
Raihanm854 <68158642+Th6man5@users.noreply.github.com>
2025-05-25 21:38:34 +0700

    Features/minor cleanup (#31)
    * feat: tambah detail jadwal
    
    * feat: API Detail FRS
    
    * feat: API Detail FRS (tahun ajar)
    
    * feat: view dashboard admin, tabel, & form
    
    * feat: view dashboard admin, tabel, & form

--- resources/assets/js/admin/table-kelas.js
+++ resources/assets/js/admin/table-kelas.js
@@ -303,7 +303,7 @@ $(function () {
         $('.card-header').after('<hr class="my-0">');
       }
     });
-    $('div.head-label').html('<h5 class="card-title mb-0">DataTable with Buttons</h5>');
+    $('div.head-label').html('<h5 class="card-title mb-0">Tabel Kelas</h5>');
   }
 
   // Delete record with route destroy
